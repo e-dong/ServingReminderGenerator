efd1e04c28fa90712aebffc4ebc44b561413246b
e-dong <eric2043@gmail.com>
2020-02-23 14:01:53 -0500

    Refactor and added comments

--- ServingReminder.js
+++ ServingReminder.js
@@ -1,3 +1,11 @@
+/**
+ * This Program creates a serving reminder as a google doc
+ * 
+ * Author: Eric Dong
+ * Creation Date: 2/22/2020
+ * Last Modfied: 2/23/2020
+ * 
+ */
 
 // IDs to access the documents
 const googleDocTemplateId = "1GXnHc9b5--0qzySO0V7uBRf0veZ4pdipwuOMcqx01qo";
@@ -16,34 +24,31 @@ const templateTexts = [
 "CBT Teacher",
 "Lunch Service"
 ];
-
-const templateCopy = DriveApp.getFileById(googleDocTemplateId).makeCopy();
-const body = DocumentApp.openById(templateCopy.getId())
-      .getBody();
   
 const createServingReminder = () => {
-  const data = oifSchedule.getDataRange().getValues().map(function(row) {
-    return row.map(function(value){ return value.toString()});
-  });
+  const data = oifSchedule.getDataRange().getValues();
 
   // Get column names
   const columnNames = data[0];
 
   // Get the appropriate Sunday Date for the reminder
-  const sundayDate = new Date();
-  sundayDate.setMonth(1);
-  sundayDate.setDate(23);
-  sundayDate.setYear(2020);
-  sundayDate.setHours(0, 0, 0);
+  // Get current Date
+  const currentDate = new Date();
   
-  // Find the current date
+  // Get data for the upcoming Sunday
   const currentRow = data.filter((d, i) =>
-    d.indexOf(sundayDate.toString()) !== -1)[0].map(row => row);
+    currentDate <= d[0] 
+  )[0].map(row => row);
   const dataMap = columnNames.reduce((acc, name, index) => {
+    // Parentheses are problematic with replacing text in the template
     name = name.replace(/[()]/g, '');
     acc[name] = currentRow[index];
     return acc;
   }, {});
+
+  // Create a copy of the google doc template
+  const templateCopy = DriveApp.getFileById(googleDocTemplateId).makeCopy();
+  const body = DocumentApp.openById(templateCopy.getId()).getBody();
 
   // Replace template text with the data in OIF Schedule
   templateTexts.forEach(text => {
